33778b55b9228918ae10b93c1d31652450c212dd
fennelouski <nathanfennel@gmail.com>
2025-10-08 18:00:05 -0600

    Initial Commit

---
+++ Racing RunTests/Racing_RunTests.swift
@@ -0,0 +1,17 @@
+//
+//  Racing_RunTests.swift
+//  Racing RunTests
+//
+//  Created by Nathan Fennel on 10/8/25.
+//
+
+import Testing
+@testable import Racing_Run
+
+struct Racing_RunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
